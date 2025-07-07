c1af92c822420b3974e32aa84116799cd0c7c169
leonardoo210399 <aditya210399@gmail.com>
2025-07-07 21:03:25 +0530

    shift client

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 // server.js
 const express   = require('express');
 const puppeteer = require('puppeteer');
+const cors    = require('cors');
 
 const app  = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(cors());
 
 // scroll helper
 async function autoScroll(page) {
